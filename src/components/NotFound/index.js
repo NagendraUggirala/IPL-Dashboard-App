304c2d640aac9404fbf987b84944b8388ec426d2
nagendrauggirala <nagendrauggirala@gmail.com>
2024-03-11 04:02:22 +0000

    solution

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const NotFound = () => {
+  return (
+    <div>
+      <img
+        className="Text-cent"
+        src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+        alt=""
+      />
+      <h1 className="Text-cent">Page Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound
